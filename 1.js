12e84b81eb646ef0f03eaa189f2d2e11446a69bf
Roshan <dalmeida.roshan@gmail.com>
2017-12-30 16:16:25 +0530

    initial

---
+++ 1.js
@@ -0,0 +1,16 @@
+
+        var a= 10;
+    
+        function add(x,y)
+        {
+            return x + y;
+        }
+        function sub(x,y)
+        {
+            return x - y;
+        }
+
+        function gel(inpID)
+        {
+            return document.getElementById(inpID);
+        }
